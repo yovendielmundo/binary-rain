290b2cf99e35a87a9a5d798e3509f3d0a67c5cb9
Luis Osorio <yovendielmundo@gmail.com>
2018-02-09 18:31:32 +0400

    add output webpack management

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,18 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
     entry: './src/sketch.js',
+    plugins: [
+        new CleanWebpackPlugin(['dist']),
+        new HtmlWebpackPlugin({
+            title: 'Binary Rain',
+            template: './src/sketch.html'
+        })
+    ],
     output: {
-        filename: 'sketch.bundle.js',
+        filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
     }
 };
