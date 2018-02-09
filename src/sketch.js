e2ffb143537c46337610f556725567c8bb45016c
Luis Osorio <yovendielmundo@gmail.com>
2018-02-09 23:17:14 +0400

    define first sketch

--- src/sketch.js
+++ src/sketch.js
@@ -1,7 +1,24 @@
-function component() {
-    var element = document.createElement('div');
-    element.innerHTML = 'Hello webpack';
-    return element;
+import 'p5';
+import Stream from './stream'
+
+let symbolSize = 24
+let symbolColor = {
+    'normal': Array(5, 124, 160), // blue green
+    'bright': Array(5, 236, 222) // green blue bright
+}
+let stream
+
+window.setup = () => {
+    createCanvas(windowWidth, windowHeight)
+    background(0)
+    textFont('Courier')
+    textSize(symbolSize)
+
+    stream = new Stream(width/2, height/2, symbolSize, symbolColor)
 }
 
-document.body.appendChild(component());
+window.draw = () => {
+    background(0, 0, 10, 150)
+
+    stream.draw()
+}
